a56e85644a9207756bba1a14eb690e20b3732c16
Sean Clark Hess <seanhess@gmail.com>
2016-05-17 10:28:37 -0600

    completed type outline

--- Numerals.hs
+++ Numerals.hs
@@ -1,22 +1,62 @@
 module Numerals where
 
--- When we have a good type system it's always best to start TDD by
+import Numeric.Natural (Natural)
+
+-- When we have a good type system it's better to begin TDD by
 -- designing the data types
 
--- natural numbers (no negative numbers)
-newtype Natural = Natural { unNatural :: Int }
-                  deriving (Show, Eq)
 
 type Thousands = Natural
 type Hundreds = Natural
 type Tens = Natural
 type Ones = Natural
 
--- intermediate representation of a number in roman form
+
+-- Intermediate representation of a number in roman form.
+-- Tally base 10 digits
 data Roman10 = Roman10 Thousands Hundreds Tens Ones
               deriving (Show, Eq)
 
--- the roman representation of a digit for output
+
+-- The roman representation of a digit for output
 data Numeral = I | V | X | L | C | D | M
              deriving (Show, Eq)
+
+
+--------------------------------------------------------------------
+-- Next we want to define the type declarations for some of the functions
+-- we will be using to make sure our design is consistent
+
+fromNatural :: Natural -> Roman10
+fromNatural = undefined
+
+toNatural :: Roman10 -> Natural
+toNatural = undefined
+
+fromNumerals :: [Numeral] -> Roman10
+fromNumerals = undefined
+
+toNumerals :: Roman10 -> [Numeral]
+toNumerals = undefined
+
+-- these will be our top-level functions
+convertArabicToRoman :: Natural -> [Numeral]
+convertArabicToRoman = undefined
+
+convertRomanToArabic :: [Numeral] -> Natural
+convertRomanToArabic = undefined
+
+-- It is tempting to make all our functions work with strings, 
+-- but we would lose type information. It's better to convert to a string
+-- at the boundary of our program. We'll stick with Numeral, Roman10, and Natural
+toString :: [Numeral] -> String
+toString = undefined
+
+fromString :: String -> Maybe [Numeral]
+fromString = undefined
+
+
+
+-- The problem is broken down into solveable pieces. We've verified that our type
+-- design makes sense. It's time to go write some tests
 
